fce183dfcba65c3b081c3936a6cfa5a752991548
YuXiang187 <59519298+YuXiang187@users.noreply.github.com>
2025-01-11 14:37:18 +0800

    Update app gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,9 +21,16 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
+    /*
+    tasks {
+        withType<JavaCompile> {
+            options.compilerArgs.add("-Xlint:deprecation")
+        }
+    }
+     */
 }
 
 dependencies {
